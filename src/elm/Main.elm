832435f0e78cffd10e7f1e8d266f9e1a9f2a7da5
Stoyan Delev <s.delev@elements.nl>
2017-08-28 07:34:14 +0200

    add list pages

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -2,7 +2,7 @@ module Main exposing (..)
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Html.Events exposing (onClick)
+import Html.Events exposing (onClick, onInput, onCheck)
 
 
 -- APP
@@ -11,6 +11,15 @@ import Html.Events exposing (onClick)
 main : Program Never Model Msg
 main =
     Html.beginnerProgram { model = model, view = view, update = update }
+
+
+type Page
+    = Login
+    | Home
+    | AddTile
+    | EditTile
+    | AddGroup
+    | EditGroup
 
 
 type TileType
@@ -62,7 +71,9 @@ type alias TypeConnection =
 
 
 type alias Model =
-    { tiles : List Tile
+    { page : Page
+    , tiles : List Tile
+    , currentTile : Maybe Tile
     , groups : List TileGroup
     , connections : List TypeConnection
     }
@@ -70,7 +81,9 @@ type alias Model =
 
 model : Model
 model =
-    { tiles = []
+    { page = Home
+    , tiles = [ emptyTile, emptyTile, emptyTile ]
+    , currentTile = Just emptyTile
     , groups = []
     , connections = []
     }
@@ -86,25 +99,69 @@ type Msg
     | SaveNewTile Tile
     | OpenExistingTile Tile
     | SaveExistingTile Tile
+    | UpdateTileField String String
 
 
 update : Msg -> Model -> Model
 update msg model =
     case msg of
+        UpdateTileField field value ->
+            let
+                newTile =
+                    case model.currentTile of
+                        Just tile ->
+                            Just (updateTile tile field value)
+
+                        _ ->
+                            model.currentTile
+            in
+                { model | currentTile = newTile }
+
         _ ->
             model
 
 
+updateTile : Tile -> String -> String -> Tile
+updateTile tile inputName inputValue =
+    case inputName of
+        "name" ->
+            { tile | name = inputValue }
+
+        "image_url" ->
+            { tile | image_url = inputValue }
+
+        "action" ->
+            { tile | action = inputValue }
+
+        "active" ->
+            { tile
+                | active =
+                    if inputValue == "True" then
+                        True
+                    else
+                        False
+            }
+
+        _ ->
+            tile
+
+
 
 -- VIEW
--- Html is defined as: elem [ attribs ][ children ]
--- CSS can be applied via class names or inline style attrib
 
 
 view : Model -> Html Msg
 view model =
     div [ class "container" ]
-        [ tileForm emptyTile
+        [ case model.page of
+            Home ->
+                div []
+                    [ listTiles model.tiles
+                    , listGroups model.groups
+                    ]
+
+            _ ->
+                text "No selected page"
         ]
 
 
@@ -126,19 +183,19 @@ tileForm tile =
     div []
         [ div [ class "form-group" ]
             [ label [] [ text "Name" ]
-            , input [ class "form-control", type_ "text", value tile.name ] []
+            , input [ class "form-control", type_ "text", value tile.name, onInput (UpdateTileField "name") ] []
             ]
         , div [ class "form-group" ]
             [ label [] [ text "Image URL" ]
-            , input [ class "form-control", type_ "text", value tile.image_url ] []
+            , input [ class "form-control", type_ "url", value tile.image_url, onInput (UpdateTileField "image_url") ] []
             ]
         , div [ class "form-group" ]
             [ label [] [ text "Action" ]
-            , input [ class "form-control", type_ "text", value tile.action ] []
+            , input [ class "form-control", type_ "text", value tile.action, onInput (UpdateTileField "action") ] []
             ]
         , div [ class "form-check" ]
             [ label [ class "form-check-label" ]
-                [ input [ class "form-check-input", type_ "checkbox", checked tile.active ]
+                [ input [ class "form-check-input", type_ "checkbox", checked tile.active, onCheck (\val -> UpdateTileField "active" (toString val)) ]
                     []
                 , text "Active"
                 ]
@@ -146,14 +203,29 @@ tileForm tile =
         ]
 
 
+listTiles : List Tile -> Html Msg
+listTiles tiles =
+    ul [] (List.map listTileItem tiles)
 
--- CSS STYLES
 
-
-styles : { img : List ( String, String ) }
-styles =
-    { img =
-        [ ( "width", "33%" )
-        , ( "border", "4px solid #337AB7" )
+listTileItem : Tile -> Html Msg
+listTileItem tile =
+    li []
+        [ h1 [] [ text tile.name ]
+        , button [] [ text "Edit" ]
+        , button [] [ text "Delete" ]
         ]
-    }
+
+
+listGroups : List TileGroup -> Html Msg
+listGroups groups =
+    ul [] (List.map listGroupItem groups)
+
+
+listGroupItem : TileGroup -> Html Msg
+listGroupItem group =
+    li []
+        [ h1 [] [ text group.name ]
+        , button [] [ text "Edit" ]
+        , button [] [ text "Delete" ]
+        ]
